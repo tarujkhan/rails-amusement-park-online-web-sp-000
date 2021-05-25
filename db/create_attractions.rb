0224dd23df4ce8ef10626c203567e647224f1b55
taroj khan <tarujkhan@yahoo.com>
2021-05-25 20:28:19 +0000

    Done.

---
+++ db/create_attractions.rb
@@ -0,0 +1,9 @@
+class CreateAttractions < ActiveRecord::Migration
+  create_table :attractions do |t|
+    t.string :name 
+    t.integer :tickets
+    t.integer :nausea_rating
+    t.integer :happiness_rating
+    t.integer :min_height
+  end
+end 
---
+++ db/create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  create_table :users do |t|
+   t.string :name 
+   t.string :password 
+   t.integer :nausea 
+   t.integer :happiness 
+   t.integer :tickets 
+   t.integer :height
+  end
+end 
