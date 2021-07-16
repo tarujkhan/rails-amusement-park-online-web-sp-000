f0e0e719d941a17bee292da095ddc1160f5b3ad7
tarujkhan <tarujkhan@yahoo.com>
2021-07-16 17:55:09 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,11 +1,14 @@
 class RidesController < ApplicationController
     def create
-        byebug
+        # byebug
         @ride = Ride.new(ride_params)
+        @user = User.find_by(name: params[:user][:name])
+        # @user = User.find(session[:user_id])
     end 
 end
 
 private
 def ride_params
-    params.require(:ride).permit(:user_id, :attraction_id)
+    params.require(:ride).permit(:user_id, :attraction_id) 
+    # if params[:ride]
 end 
