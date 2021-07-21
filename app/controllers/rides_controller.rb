c5d3f2721d90f8697eb40e49315fcd44c3093b0a
tarujkhan <tarujkhan@yahoo.com>
2021-07-21 16:47:07 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,9 +7,9 @@ class RidesController < ApplicationController
         # @result = @ride.take_ride
         # byebug
         @take_ride = @ride.take_ride
-        redirect_to user_path(@ride.user), notice: "Thanks for riding the #{@attraction.name}"
+        redirect_to user_path(@ride.user), flash[:message] = @take_ride
         # @user = User.find(session[:user_id])
-        # check for tickets check if user is tall enough @take_it = take_ride
+        # check for tickets check if user is tal enough @take_it = take_ride
         #  flash: #{message:Thanks for ridingthe } - save to a variable the return value of take ride
         # these steps should be done before the redirect and attach the flash message with the redirect
         # in user show display flash message
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,14 +3,18 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
   
   def take_ride
+    # byebug
     if self.attraction.tickets > self.user.tickets && self.attraction.min_height > self.user.height
       "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
-      elsif self.attraction.tickets > self.user.tickets
+    elsif self.attraction.tickets > self.user.tickets
       "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-      elsif self.attraction.min_height > self.user.height
+    elsif self.attraction.min_height > self.user.height
       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
-    # else
-      # self.user.update(tickets: self.user.tickets - self.attraction.tickets, nausea: self.user.nausea + self.attraction.nausea_rating, happiness: self.user.happiness + self.attraction.happiness_rating)
+    elsif
+      self.user.update(tickets: self.user.tickets - self.attraction.tickets, nausea: self.user.nausea + self.attraction.nausea_rating, happiness: self.user.happiness + self.attraction.happiness_rating)
+      "Thanks for riding the #{self.attraction.name}"
+    # else 
+        # redirect_to attraction_path(attraction)
     end
   end
 end
